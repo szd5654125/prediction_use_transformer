d1461217efd36e391e6bec2faea23252d0201842
Kailun Qiu <szd5654125@gmail.com>
2025-02-03 17:16:21 +0800

    修改了一个小bug

--- code/set_target.py
+++ code/set_target.py
@@ -43,7 +43,7 @@ def detect_trend(df, cost=0.00036):
                             highest_price = current_close
                         # 如果回撤超过交易成本代表趋势结束，填上最大值到之前所有位置的趋势数
                         if (highest_price - current_close) / highest_price > cost:
-                            for trend_index in [i, highest_index]:
+                            for trend_index in range(i, highest_index):
                                 trend_returns[trend_index] = (highest_price - df.iloc[trend_index]['close']) / df.iloc[trend_index]['close']
                             break  # 出现 0.036% 的回撤，结束趋势
                 else:
@@ -57,7 +57,7 @@ def detect_trend(df, cost=0.00036):
                             lowest_price = current_close
                         # 如果回撤超过交易成本代表趋势结束，填上最小值到之前所有位置的趋势数
                         if (current_close - lowest_price) / lowest_price > cost:
-                            for trend_index in [i, lowest_index]:
+                            for trend_index in range(i, lowest_index):
                                 trend_returns[trend_index] = (df.iloc[trend_index]['close'] - lowest_price) / df.iloc[trend_index]['close']
                             break
         i = max(lowest_index, highest_index) + 1  # 移动 i 到下一个未处理的位置
